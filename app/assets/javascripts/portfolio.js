2cc6417a2dd9257c859c5f780088ddef0d5f65c2
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-06-18 22:26:16 -0400

    Basic view.

--- app/assets/javascripts/portfolio.js
+++ app/assets/javascripts/portfolio.js
@@ -1,5 +1,16 @@
 Portfolio = Ember.Application.create({
   ready: function() {
-           alert("What.");
          }
+});
+
+Portfolio.Quote = Ember.Object.extend({
+  symbol: null,
+  price: null
+});
+
+Portfolio.quotesController = Ember.ArrayController.create({
+  content: [
+    Portfolio.Quote.create({ 'symbol': 'AAPL', 'price': 500.00}),
+    Portfolio.Quote.create({ 'symbol': 'VTI',  'price': 75.00})
+  ]
 });
