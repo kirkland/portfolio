aa92b95782629c072af129fd1b1de038ede3dc3e
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-07-01 15:46:19 -0400

    Find by session_id.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   def app
     @session_id = request.session_options[:id]
-    @portfolio_data = Portfolio.find_by_user_id @session_id
+    @portfolio_data = Portfolio.find_by_session_id @session_id
   end
 end
